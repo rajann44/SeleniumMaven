55d94d9847c944df44b42f29d0d7bcdb63a2f2ef
Rajan Chaudhary <rajann44@gmail.com>
2022-08-31 00:59:51 +0530

    add some method in basepage

--- src/main/java/page/BasePage.java
+++ src/main/java/page/BasePage.java
@@ -1,27 +1,75 @@
 package page;
 
 import com.aventstack.extentreports.ExtentTest;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class BasePage {
 
     WebDriver driver;
+    protected Actions action = new Actions(this.driver);
     static ThreadLocal<ExtentTest> logger;
 
-    public BasePage(WebDriver driver){
+    public BasePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public static void setLogger(ThreadLocal<ExtentTest> extentTestThreadLocal){
+    public static void setLogger(ThreadLocal<ExtentTest> extentTestThreadLocal) {
         logger = extentTestThreadLocal;
     }
 
-    public void logInfo(String logInfo){
+    public void logInfo(String logInfo) {
         logger.get().info(logInfo);
     }
 
-    public void gotToURL(String url){
+    /**
+     * Go to the specified url
+     *
+     * @param url
+     */
+    public void gotToURL(String url) {
         driver.get(url);
+    }
+
+    /**
+     * Refreshes the browser driver
+     */
+    public void refreshBrowser() {
+        driver.navigate().refresh();
+    }
+
+    /**
+     * This Method is used to drag and drop element
+     *
+     * @param element
+     * @param toElement
+     * @throws InterruptedException
+     */
+    public void dragAndDrop(WebElement element, WebElement toElement) {
+        action.dragAndDrop(element, toElement).build().perform();
+    }
+
+    /**
+     * This method will hold shift and click on element
+     *
+     * @param element
+     */
+    public void shiftClick(WebElement element) {
+        Actions shiftClick = new Actions(driver);
+        shiftClick.keyDown(Keys.SHIFT).click(element).keyUp(Keys.SHIFT).perform();
+    }
+
+    /**
+     * Moves to a web element with WebElement
+     *
+     * @param element
+     * @return WebElement
+     */
+    public WebElement moveToElement(WebElement element) {
+        new Actions(driver).moveToElement(element).build().perform();
+        return element;
     }
 
 
