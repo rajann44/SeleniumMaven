3d8bc05ce518dcb2a8499ea513f54947a5e18cac
Rajan Chaudhary <rajann44@gmail.com>
2022-08-31 14:40:51 +0530

    update chrome argument

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -28,6 +28,7 @@ public class DriverManager {
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
                 options.addArguments("--headless");
+                options.addArguments("--remote-debugging-port=9222");
                 setDriver(WebDriverManager.chromedriver().capabilities(options).create());
                 break;
             default:
