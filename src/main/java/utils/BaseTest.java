54a44a4e0b513307a86ec6e8276447934810ff35
Rajan Chaudhary <rajann44@gmail.com>
2022-10-18 22:16:45 +0530

    log file support

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -5,10 +5,7 @@ import helper.GSheetHelper;
 import helper.TestListeners;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
@@ -19,7 +16,8 @@ public class BaseTest extends DriverManager{
 
     @BeforeSuite(alwaysRun = true)
     public void setData(){
-        consoleLogger.warn("BEFORE SUIT: Getting Data from Sheet and setting Keys.....");
+        consoleLogger.info("********* Automation Execution Started **********");
+        consoleLogger.info("BEFORE SUIT: Getting Data from Sheet and setting Keys.....");
         GSheetHelper.setTestDataFromSheet(GSheetKeys.sheetURL);
     }
 
@@ -35,6 +33,11 @@ public class BaseTest extends DriverManager{
         consoleLogger.info("AFTER METHOD: Quit Driver and Destroy Driver Thread.....");
         getDriver().quit();
         this.driver.remove();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void cleanup(){
+        consoleLogger.info("********* Automation Execution Completed **********");
     }
 
 }
