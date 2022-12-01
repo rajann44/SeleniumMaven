74b068ab52b855c4ca11b2630faabab8eb8ed62d
Rajan Chaudhary <rajann44@gmail.com>
2022-12-01 20:33:39 +0530

    unused import

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -3,7 +3,6 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverManager {
 
